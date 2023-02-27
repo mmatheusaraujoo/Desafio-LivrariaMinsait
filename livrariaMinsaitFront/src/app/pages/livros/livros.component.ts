994c14a3e5fda17fc6b5b88de2112eb2942f75ee
mmatheusaraujoo <matheus10_araujo@hotmail.com>
2023-02-27 03:50:09 -0300

    AlterandoFront
    adicionando novas paginas

--- livrariaMinsaitFront/src/app/pages/livros/livros.component.ts
+++ livrariaMinsaitFront/src/app/pages/livros/livros.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { livro } from 'src/app/models/livro.models';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-livros',
   templateUrl: './livros.component.html',
   styleUrls: ['./livros.component.css']
 })
-export class LivrosComponent {
+export class LivrosComponent implements OnInit {
 
+  public livros$!: Observable<livro[]>;
+
+  constructor(private data: DataService) {
+
+  }
+
+  async ngOnInit() {
+    this.livros$ = await this.data.retornarLivros();;
+  }
 }
